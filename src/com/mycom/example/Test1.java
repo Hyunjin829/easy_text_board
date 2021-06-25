7102f72ad38fba026dac0b1b4251171a0f31182f
Hyunjin829 <gus10010@naver.com>
2021-06-25 15:05:04 +0900

    Test

---
+++ src/com/mycom/example/Test1.java
@@ -0,0 +1,8 @@
+package com.mycom.example;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/com/mycom/example/Test1000.java
@@ -0,0 +1,13 @@
+package com.mycom.example;
+
+import java.util.Scanner;
+
+public class Test1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x, y;
+		x = sc.nextInt();
+		y = sc.nextInt();
+		System.out.println(x+y);
+	}
+}
