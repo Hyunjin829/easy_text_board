2728949739e459a4e119c5b85fe72b4eee836af4
Hyunjin829 <gus10010@naver.com>
2021-06-25 15:52:39 +0900

    Test7

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int b100 = (b/100)%10;
-		int b10 = (b/10)%10;
-		int b1 = b%10;
-		
-		System.out.println(a*b1);
-		System.out.println(a*b10);
-		System.out.println(a*b100);
-		System.out.println((a*b1)+(a*b10)*10+(a*b100)*100);
+		System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+
 		
 	} // main
 } // Class
