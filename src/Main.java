eeec03d2aeede9f2ae85d6e53ee2f523b7761dc4
Hyunjin829 <gus10010@naver.com>
2021-06-26 12:52:26 +0900

    Test4

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,22 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int year = sc.nextInt();
+		int x = sc.nextInt();
+		int y = sc.nextInt();
 		sc.close();
 		
-		if (year%4 == 0) {
-			if (year%100 != 0) {
-				System.out.println("1");
-			} else if (year%400 == 0) {
+		if (x > 0) {
+			if (y > 0) {
 				System.out.println("1");
 			} else {
-				System.out.println("0");
+				System.out.println("4");
 			}
 		} else {
-			System.out.println("0");
+			if (y > 0) {
+				System.out.println("2");
+			} else {
+				System.out.println("3");
+			}
 		}
 		
 	} // main
