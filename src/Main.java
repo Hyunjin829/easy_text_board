3a1e89365bbcba8bf5cd322e4bf368ce59b6f27d
Hyunjin829 <gus10010@naver.com>
2021-06-27 21:46:31 +0900

    Test1

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int H = sc.nextInt();
-		int M = sc.nextInt();
+		int n = sc.nextInt();
 		sc.close();
 		
-		M = M - 45;
-		
-		if(M<0) {
-			H = H -1;
-			M = M + 60;
-			if(H<0) {
-				H = H + 24;
-			}
+		for (int i = 1; i < 10; i++) {
+			System.out.println( n + " * " + i + " = " + n*i);
 		}
-		
-//		if (H<0 || M<0) {
-//			if (H<0 && M>0) {
-//				H = H + 24;
-//			} else if (H>0 && M<0) {
-//				M = M + 60;
-//			} else {
-//				H = H + 24;
-//				M = M + 60;
-//			}
-//		}
-//		
-		System.out.printf("%d %d", H, M);
 		
 	} // main
 } // Class
