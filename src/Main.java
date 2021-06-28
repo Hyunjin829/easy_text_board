5f8b8c5618657ea257a47003b64364a060dd52a9
Hyunjin829 <gus10010@naver.com>
2021-06-28 22:33:22 +0900

    Test8

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,22 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int X = Integer.parseInt(st.nextToken());
 		
-		
-		String A = br.readLine();
-		String arr[] = A.split(" ");
-		
-		for (int i = 0; i < arr.length; i++) {
-			if (Integer.parseInt(arr[i]) < X) {
-				System.out.print(arr[i] + " ");
-			}
+		String arr[] = br.readLine().split(" ");
+		int N = Integer.parseInt(arr[0]);
+		int X = Integer.parseInt(arr[1]);
+		arr = br.readLine().split(" ");
+		for (int i = 0; i < N; i++) {
+			int Y = Integer.parseInt(arr[i]);
+			if(Y < X)
+				bw.write(Y + " ");
+			
 		}
 		
 		br.close();
