7dd16120d26baddd0b73b2256c90563474affeb3
Hyunjin829 <gus10010@naver.com>
2021-06-29 14:29:07 +0900

    Test2

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,34 @@ import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String arr[];
-		
-		while (true) {
-			arr = br.readLine().split(" ");
-			int A = Integer.parseInt(arr[0]);
-			int B = Integer.parseInt(arr[1]);
-			if( A==0 && B==0)
-				break;
-			bw.write((A+B) + "\n");
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			String str;
+			
+			while ((str=br.readLine()) != null) {
+				String[] arr = br.readLine().split(" ");
+				int A = Integer.parseInt(arr[0]);
+				int B = Integer.parseInt(arr[1]);
+				bw.write((A+B) + "\n");
+			}
+			br.close();
+			bw.flush();
+			bw.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
 		}
-		br.close();
-		bw.close();
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		String s;
+//		
+//		while((s=br.readLine()) != null) {
+//			StringTokenizer st = new StringTokenizer(s, " ");
+//			int a = Integer.parseInt(st.nextToken());
+//			int b = Integer.parseInt(st.nextToken());
+//			sb.append(a+b).append('\n');
+//		}
+//		System.out.println(sb);
 	} // main
 } // Class
--- src/Main2.java
+++ src/Main2.java
@@ -1,24 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main2 {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int score = sc.nextInt();
-		
-		if (score >= 90) {
-			System.out.println("A");
-		} else if (score >= 80) {
-			System.out.println("B");
-		} else if (score >= 70) {
-			System.out.println("C");
-		} else if (score >= 60) {
-			System.out.println("D");
-		} else {
-			System.out.println("F");
-		}
-		
-		
-	} // main
-} // Class
+	 public static void main(String[] args) throws IOException {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        StringBuilder sb = new StringBuilder();
+	        String s = "";
+
+	        while((s = br.readLine()) != null){
+	            StringTokenizer st = new StringTokenizer(s, " ");
+
+	            int a = Integer.parseInt(st.nextToken());
+	            int b = Integer.parseInt(st.nextToken());
+
+	            sb.append(a+b).append('\n');
+	        }
+
+	        System.out.println(sb);
+	    }
+	}
