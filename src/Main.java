79faa75c0dec513a63e527a6abae83f4c9f20b8c
Hyunjin829 <gus10010@naver.com>
2021-06-29 17:35:49 +0900

    Test3

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,28 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-			String str;
-			
-			while ((str=br.readLine()) != null) {
-				String[] arr = br.readLine().split(" ");
-				int A = Integer.parseInt(arr[0]);
-				int B = Integer.parseInt(arr[1]);
-				bw.write((A+B) + "\n");
-			}
-			br.close();
-			bw.flush();
-			bw.close();
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int M = N;
+		int count = 0;
+
+		if (N < 10) {
+			N *= 10;
+			M = N;
 		}
-		
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringBuilder sb = new StringBuilder();
-//		String s;
-//		
-//		while((s=br.readLine()) != null) {
-//			StringTokenizer st = new StringTokenizer(s, " ");
-//			int a = Integer.parseInt(st.nextToken());
-//			int b = Integer.parseInt(st.nextToken());
-//			sb.append(a+b).append('\n');
-//		}
-//		System.out.println(sb);
+		while (true) {
+			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+			count++;
+			if (M == N) {
+				break;
+			}
+		}
+		System.out.println(count);
+
 	} // main
 } // Class
