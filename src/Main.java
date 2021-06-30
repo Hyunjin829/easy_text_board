1be4c757ff440d38699e7620e4d4dc51301449cb
Hyunjin829 <gus10010@naver.com>
2021-06-30 20:24:35 +0900

    Test2

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-		String A = br.readLine();
-		String arr[] = A.split(" ");
-		br.close();
-
-		int max = Integer.parseInt(arr[0]);
-		int min = Integer.parseInt(arr[0]);
+		
+		int arr[] = new int[9];
+		int max = 0, num = 0;
 		for (int i = 0; i < arr.length; i++) {
-			if (max - Integer.parseInt(arr[i]) < 0) {
-				max = Integer.parseInt(arr[i]);
-			}
-			if (min - Integer.parseInt(arr[i]) > 0) {
-				min = Integer.parseInt(arr[i]);
+			arr[i] = Integer.parseInt(br.readLine());
+			if (max - arr[i] < 0) {
+				max = arr[0];
+				max = arr[i];
+				num  = i+1;
 			}
 		}
-		System.out.println(min + " " + max);
-
+		System.out.println(max);
+		System.out.println(num);
 	} // main
 } // Class
