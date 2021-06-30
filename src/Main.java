b5e60b3dfebc95d379ef5c4bb0ca17fdd8c4040f
Hyunjin829 <gus10010@naver.com>
2021-06-30 20:59:10 +0900

    Test3

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,22 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int arr[] = new int[9];
-		int max = 0, num = 0;
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			if (max - arr[i] < 0) {
-				max = arr[0];
-				max = arr[i];
-				num  = i+1;
+
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		int x = A * B * C;
+		int arr[] = new int[10];
+		for (int i = 1; x / i != 0; i = i * 10) {
+			for (int j = 0; j < 10; j++) {
+				if ((x / i) % 10 == j) {
+					arr[j] += 1;
+				}
 			}
 		}
-		System.out.println(max);
-		System.out.println(num);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
 	} // main
 } // Class
