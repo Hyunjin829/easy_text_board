08c3af643d8646b05308b15c17e7e2d056ec49a8
Hyunjin829 <gus10010@naver.com>
2021-07-01 19:44:05 +0900

    Test1

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
 
-		int A = Integer.parseInt(br.readLine());
-		int B = Integer.parseInt(br.readLine());
-		int C = Integer.parseInt(br.readLine());
-		int x = A * B * C;
+		Scanner sc = new Scanner(System.in);
 		int arr[] = new int[10];
-		for (int i = 1; x / i != 0; i = i * 10) {
-			for (int j = 0; j < 10; j++) {
-				if ((x / i) % 10 == j) {
-					arr[j] += 1;
+		int num = 10;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt() % 42;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					num--;
+					break;
 				}
 			}
 		}
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
-		}
+		System.out.println(num);
 
 	} // main
 } // Class
