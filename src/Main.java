a20db83596ff1f585d756cee6857db372fc14139
Hyunjin829 <gus10010@naver.com>
2021-07-01 20:23:32 +0900

    Test2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int arr[] = new int[10];
-		int num = 10;
+
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		int max;
+		double sum;
+
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt() % 42;
-			for (int j = 0; j < i; j++) {
-				if (arr[i] == arr[j]) {
-					num--;
-					break;
-				}
-			}
+			arr[i] = sc.nextInt();
 		}
-		System.out.println(num);
+		Arrays.sort(arr);
+		max = arr[N - 1];
+		sum = 0;
+		
+		for (int i = 0; i < N; i++) {
+//			System.out.println(arr[i]);
+//			System.out.println(((double)arr[i] / (double)max) * 100);
+			sum += ((double)arr[i] / (double)max) * 100;
+//			System.out.println("sum: "+sum);
+		}
+		System.out.println(sum/(double)N);
+
 
 	} // main
 } // Class
