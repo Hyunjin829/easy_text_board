86aebf84197d9c19f482a65376805074208e1f15
Hyunjin829 <gus10010@naver.com>
2021-07-04 21:38:26 +0900

    Test0

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		String[] arr;
-		
-		for (int i = 0; i < T; i++) {
-			arr = br.readLine().split("");
-			int sum = 0, hap = 1;
-			for (int j = 0; j < arr.length; j++) {
-				if (arr[j].equals("O")) {
-					sum += hap;
-					hap++;
-				} else {
-					hap = 1;
+		int C = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < C; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int N = Integer.parseInt(st.nextToken());
+			int[] arr = new int[N];
+			int r = 0;
+
+			while (true) {
+				arr[r] = Integer.parseInt(st.nextToken());
+//				System.out.println(arr[r]);
+				r++;
+				if (r == N) {
+					int sum = 0;
+					for (int j = 0; j < arr.length; j++) 
+						sum += arr[j];
+					System.out.println(sum);
+					
+					break;
 				}
 			}
-			System.out.println(sum);
+			
+			
 		}
-		
-		
 
 	} // main
 } // Class
