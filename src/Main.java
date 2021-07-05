b7a72d8b0e27f1b63c7891fd0ff4e43088b3e868
Hyunjin829 <gus10010@naver.com>
2021-07-05 15:24:29 +0900

    Test1

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,41 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int C = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine()); // 테스트케이스 입력
+		StringTokenizer st;
+		int N, r, sum;
+		int[] arr;
+		double avg;
+		double[] high;
 
-		for (int i = 0; i < C; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int N = Integer.parseInt(st.nextToken());
-			int[] arr = new int[N];
-			int r = 0;
+		for (int i = 0; i < C; i++) { // C까지 반복(0~4)
 
+			st = new StringTokenizer(br.readLine(), " ");
+			N = Integer.parseInt(st.nextToken()); // 점수 수 N 입력
+			arr = new int[N];
+			high = new double[C];
+
+			r = 0;
 			while (true) {
 				arr[r] = Integer.parseInt(st.nextToken());
-//				System.out.println(arr[r]);
 				r++;
+				sum = 0;
+
 				if (r == N) {
-					int sum = 0;
-					for (int j = 0; j < arr.length; j++) {
+					for (int j = 0; j < N; j++) {
 						sum += arr[j];
 					}
-					System.out.println(sum);
-					
+					avg = sum / N;
+					for (int j = 0; j < N; j++) {
+						if (arr[j] > avg) {
+							high[i]++;
+						} // 평균 이상 인원 덧셈
+					}
 					break;
-				}
-			}
-			
-			
-		}
+				} // N만큼 입력받은 후 입력 종료
+			} // N개의 점수 받기
+			System.out.println(String.format("%.3f", (high[i] / N) * 100) + "%");
+		} // for
 
 	} // main
 } // Class
--- src/Main2.java
+++ src/Main2.java
@@ -4,20 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main2 {
-	 public static void main(String[] args) throws IOException {
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        StringBuilder sb = new StringBuilder();
-	        String s = "";
+	public static void main(String[] args) throws IOException {
 
-	        while((s = br.readLine()) != null){
-	            StringTokenizer st = new StringTokenizer(s, " ");
-
-	            int a = Integer.parseInt(st.nextToken());
-	            int b = Integer.parseInt(st.nextToken());
-
-	            sb.append(a+b).append('\n');
-	        }
-
-	        System.out.println(sb);
-	    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int C = Integer.parseInt(br.readLine());
+		int N = 0;
+		
+		for (int i = 0; i < C; i++) {
+			int count = 0;
+			int totalScore = 0;
+			double avg = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			int[] score = new int[N];
+			
+			for (int j = 0; j < N; j++) {
+				score[j] = Integer.parseInt(st.nextToken());
+				totalScore += score[j];
+			}
+			
+			avg = (double)totalScore / (double)N;
+			
+			for (int j = 0; j < score.length; j++) {
+				if (score[j] > avg) {
+					count++;
+				}
+			}
+			
+			System.out.println(String.format("%.3f", ((double)count/N) * 100) + "%");
+			
+		}
+		
 	}
+}
