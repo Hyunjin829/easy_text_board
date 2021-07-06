b8a8428f30d4b1959705791266aeb2cd01256365
Hyunjin829 <gus10010@naver.com>
2021-07-06 20:11:23 +0900

    1065

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	
-	public static int d(int n) {
-		int sum = n;
-		
-		while(n != 0) {
-			sum += n % 10;
-			n = n / 10;
+
+	public static boolean chk(int n) {
+		String number = Integer.toString(n);
+		int[] arr = new int[number.length()];
+		for (int i = 0; i < number.length(); i++) {
+			arr[i] = Integer.parseInt(number.charAt(i) + "");
 		}
-		
-		return sum;
+		int i=number.length();
+		boolean ans = false;
+		while (true) {
+			if (i <= 2) {
+				ans = true;
+			} else {
+				ans = (arr[i-1]-arr[i-2])==(arr[i-2]-arr[i-3]) ? true : false;
+				i--;
+			}
+			if (i - 3 < 0)
+				break;
+		}
+		return ans;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String n = br.readLine();
+		boolean[] check = new boolean[Integer.parseInt(n)];
+		int sum = 0;
 
-		boolean[] check = new boolean[10001];
-
-		for (int i = 1; i < check.length; i++) {
-			int n = d(i);
-
-			if (n < check.length) {
-				check[n] = true;
+		for (int i = 0; i < Integer.parseInt(n); i++) {
+			check[i] = chk(i+1);
+			if (check[i]) {
+				sum++;
 			}
 		}
-
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 1; i < check.length; i++) {
-			if (!check[i]) {
-				sb.append(i).append('\n');
-			}
-		}
-		System.out.println(sb);
+		System.out.println(sum);
 
 	} // main
-	
+
 } // Class
--- src/Test.java
+++ src/Test.java
@@ -1,21 +1,17 @@
 public class Test {
 
 	public static void main(String[] args) {
+		int n = 1492;
+		int sum = n;
 		
-		int n = 1457;
-		int n1 = (n/1)%10;
-		int n10 = (n/10)%10; 
-		int n100 = (n/100)%10; 
-		int n1000 = (n/1000)%10; 
-		
-		for (int i = 1; i < 10000; i *= 10) {
-			System.out.println(i);
+		while(n != 0) {
+			sum += n % 10;
+			System.out.println("sum" + sum);
+			n = n / 10;
+			System.out.println("n" + n);
 		}
 		
-		System.out.println(n1);
-		System.out.println(n10);
-		System.out.println(n100);
-		System.out.println(n1000);
+		System.out.println(sum);;
 
 	}
 
